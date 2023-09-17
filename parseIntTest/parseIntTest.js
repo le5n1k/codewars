649a2edf032ea7539197aa6170e1c542380c3c4c
le5n1k <3dimon343@gmail.com>
2023-09-17 14:44:51 +0300

    add 7kyu_FindAnEmployeesRoleInTheCompany.js testing parseIntTest.js

--- parseIntTest/parseIntTest.js
+++ parseIntTest/parseIntTest.js
@@ -1,1 +1,4 @@
-console.log(parseInt("11110000",2)); // 240 из бинарного в десятичный
+//console.log(parseInt("11110000",2)); // 240 из бинарного в десятичный
+
+let employees = [ {firstName: "Dipper", lastName: "Pines", role: "Boss"}];
+console.log(employees[0].firstName);
