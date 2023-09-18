8908b619cbf3afa151f5083bad82c5b2565f0547
le5n1k <3dimon343@gmail.com>
2023-09-18 14:11:41 +0300

    add 7kyu_FindFactorsOfANumber.js

---
+++ 7kyu_FindFactorsOfANumber/7kyu_FindFactorsOfANumber.js
@@ -0,0 +1,22 @@
+/*
+Create a function that takes a number and finds the factors of it, listing them in descending order in an array.
+
+If the parameter is not an integer or less than 1, return -1. In C# return an empty array.
+
+For Example: factors(54) should return [54, 27, 18, 9, 6, 3, 2, 1]
+ */
+
+function factors(x){
+    let arr=[];
+    if(!Number.isInteger(x) || x<0 || x===0) return -1;
+    for(let i=1;i<x;i++) {
+        if(x%i===0) {
+            arr.push(i);
+        }
+    }
+    arr.push(x);
+    arr.reverse();
+    return arr;
+}
+
+console.log(factors(0));
